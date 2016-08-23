40d7132622ef21ab95e9d54dede4cf4538b0fbaa
Karen <kareninbootcamp@gmail.com>
2016-08-23 17:04:25 -0400

    Bank Account Changes

--- src/ssa/Account.java
+++ src/ssa/Account.java
@@ -2,26 +2,79 @@ package ssa;
 
 public class Account {
 	
-	int id;
-	String description;
-	double balance;
+	private int id;
+	private String description;
+	private double balance;
 	
-	
-	
+	//constructor
+		public Account(int id, String description){
+			this.id = id;
+			this.description = description;
+			this.balance = 0;
+		}
+		//constructor for description
+		public Account(String description){
+			this.description = description;
+			this.balance = 0;
+		}
+		Account() {
+			this.description = "default account description";
+			this.balance = 0;
+				
+		}
+		
 	double deposit(double aDeposit){
 		balance = balance + aDeposit;
 		return balance;
 	}
 	
-	double withdrawal(double aWithdrawal){
-		if(aWithdrawal > balance) {
+	double withdraw(double aWithdraw){
+		if(aWithdraw > balance) {
 			System.out.println("Insufficient Funds!");
 		} else 
 			
-			balance = balance - aWithdrawal;
+			balance = balance - aWithdraw;
 			return balance;
 			
 		}
+	//transfer method
+	public void transferFrom(Account account, double amount) {
+		if (account.balance > amount){
+			account.withdraw(amount);
+				this.deposit(amount);
+				
+			} else 
+				System.out.println("Transfer Fail!");
+		}
+		
+	
+	
+	public int getId(){
+		return id;
+	}
+	void setId(int anId){
+		 this.id = anId;		
+	}
+	
+	
+	//getter for description
+	String getDescription() {
+		  return description;
+		}
+	//setter for description
+	void setDescription(String aDescription) {
+		  this.description = aDescription;
+		}
+
+	
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
 	
 	
 		
